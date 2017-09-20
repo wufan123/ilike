147b5bf94f64984c63cd8686c2de912668c67972
whr <357114638@qq.com>
2017-09-20 17:22:25 +0800

    选座控件 android端

--- android/app/src/main/java/com/zmfilm/MainApplication.java
+++ android/app/src/main/java/com/zmfilm/MainApplication.java
@@ -2,55 +2,47 @@ package com.zmfilm;
 
 import android.app.Application;
 
-import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactApplication;
+import cn.reactnative.alipay.AlipayPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.theweflex.react.WeChatPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.theweflex.react.WeChatPackage;
-import com.zmax.ZmaxReactPackage;
-
-import org.pgsqlite.SQLitePluginPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
-import cn.jpush.reactnativejpush.JPushPackage;
-import cn.reactnative.alipay.AlipayPackage;
-
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
-                    new MainReactPackage(),
-                    new JPushPackage(true, true),
-                    new AlipayPackage(),
-                    new ReactVideoPackage(),
-                    new RCTCameraPackage(),
-                    new WeChatPackage(),
-                    new SQLitePluginPackage(),
-                    new ZmaxReactPackage()
-            );
-        }
-    };
-
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new AlipayPackage(),
+            new ReactVideoPackage(),
+            new RCTCameraPackage(),
+            new WeChatPackage()
+      );
     }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -8,13 +8,16 @@ import {
   Image,
   Dimensions,
   StyleSheet,
-  FlatList
+  FlatList,
+  Platform,
+  SectionList
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-import SeatSelectView from '../common/SeatSelectView'
 import * as WeChat from 'react-native-wechat';
-import Header from '../common/header'
+import Header from '../common/header';
 import Swiper from 'react-native-swiper';
+import { RefreshScrollView } from '../common/pull'
+import globalStyle from '../../style/index'
 
 const { width, height } = Dimensions.get('window')
 
@@ -49,30 +52,71 @@ class HomeScreen extends Component {
         6,
         7,
         8
-      ]
-    }
+      ],
+      commingMovies: [
+        {data:[1, 2, 3,4,5], title: 'section 0'},
+        {data:[1, 2, 3], title: 'section 1'},
+        {data:[1, 2, 3,4], title: 'section 2'},
+        {data:[1, 2,], title: 'section 3'},
+      ],
+      swiperShow: false,
+      curTab: 0
+    },
+    this.hotList = null,
+    this.commingList = null
   }
-  changeSelect(selectItem) { }
+  changeSelect(selectItem) { 
+    let curTab = 0;
+    switch(selectItem) {
+      case '热映':
+        curTab = 0;
+        break;
+      case '待映':
+        curTab = 1;
+        break;
+    }
+    this.setState({
+      curTab: curTab
+    })
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        swiperShow: true
+      })
+    }, 0)
+  }
 
   _header = () => {
-    return (
-      <View style={styles.swiperContainer}>
-        <Swiper autoplay={true} paginationStyle={styles.pagination} activeDotColor="#fff">
+    if (this.state.swiperShow) {
+      return (
+        <View style={styles.swiperContainer}>
+          <Swiper autoplay={true} paginationStyle={styles.pagination} activeDotColor="#fff">
+            <View style={styles.slide}>
+              <Image resizeMode="stretch" style={styles.adImage} source={require('../../assets/lake.png')} />
+            </View>
+            <View style={styles.slide}>
+              <Image resizeMode="stretch" style={styles.adImage} source={require('../../assets/lake.png')} />
+            </View>
+          </Swiper>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.swiperContainer}>
           <View style={styles.slide}>
             <Image resizeMode="stretch" style={styles.adImage} source={require('../../assets/lake.png')} />
           </View>
-          <View style={styles.slide}>
-            <Image resizeMode="stretch" style={styles.adImage} source={require('../../assets/lake.png')} />
-          </View>
-        </Swiper>
-      </View>
-    );
+        </View>
+      )
+    }
   }
 
   _footer = () => {
     return (
-      <View>
-        <Text>at the end</Text>
+      <View style={styles.footer}>
+        <Text style={{ color: globalStyle.fontColorGray, fontSize: 12 }}>已经到底啦～</Text>
       </View>
     )
   }
@@ -103,16 +147,27 @@ class HomeScreen extends Component {
             flex: 1,
             flexDirection: 'row',
             justifyContent: 'space-between',
-            marginTop: 15
+            ...Platform.select({
+              ios: {
+                marginTop: 15
+              },
+              android: {
+                marginTop: 8
+              }
+            })
           }}>
-            <View>
-              <Text style={styles.movieSlogan}>天堂实假象，险象险中还</Text>
-              <Text style={styles.movieActress}>迈克尔格林／约翰·洛根／杰</Text>
+            <View style={{
+              flex: 1,
+              justifyContent: 'space-between',
+              alignSelf: 'stretch'
+            }}>
+              <Text style={styles.movieSlogan}><Text>导演： </Text>天堂实假象，险象险中还</Text>
+              <Text style={styles.movieActress} numberOfLines={1}><Text style={globalStyle.fontBalck}>主演： </Text>迈克尔格林／约翰·洛根／杰迈克尔格林／约翰·洛根／杰迈克尔格林／约翰·洛根／杰迈克尔格林／约翰·洛根／杰迈克尔格林／约翰·洛根／杰迈克尔格林／约翰·洛根／杰</Text>
             </View>
-            <View>
+            <View style={{width: 46,}}>
               <TouchableOpacity style={styles.buyButton} onPress={() => { }}>
                 <Text style={{
-                  color: '#dc3c38'
+                  color: globalStyle.colorPrimary
                 }}>购票</Text>
               </TouchableOpacity>
             </View>
@@ -122,18 +177,140 @@ class HomeScreen extends Component {
     )
   }
 
+  _renderCommingMovieItem = ({item}) => {
+    let collectIcon = false ? require('../../assets/home/icon_collect.png') : require('../../assets/home/icon_uncollect.png')
+    return (
+      <View style={styles.row}>
+        <View style={styles.movieThumbContainer}>
+          <Image resizeMode="stretch" style={styles.movieThumb} source={{
+            uri: 'http://img5.mtime.cn/pi/2017/03/23/233340.20916876_1000X1000.jpg'
+          }} />
+        </View>
+        <View style={styles.movieDetailContainer}>
+          <View style={styles.titleScoreContainer}>
+            <Text style={styles.movieTitle}>异形：契约</Text>
+            <Text style={[globalStyle.fontGray, globalStyle.font14]}>98min</Text>
+          </View>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            ...Platform.select({
+              ios: {
+                marginTop: 15
+              },
+              android: {
+                marginTop: 8
+              }
+            })
+          }}>
+            <View style={{
+              flex: 1,
+              justifyContent: 'space-between',
+              alignSelf: 'stretch'
+            }}>
+              <Text style={styles.movieSlogan}><Text>主演： </Text>迈克尔格林／约翰·洛根／杰</Text>
+              <View style={{flexDirection:'row', alignItems:'center'}}>
+                <Image
+                  source={require('../../assets/home/icon_like.png')}
+                  style={{width: 8, height: 12}}
+                />
+                <Text style={[globalStyle.fontOrange, globalStyle.font12]} numberOfLines={1}>166影迷关注</Text>
+              </View>
+            </View>
+            <View style={{width: 46,}}>
+              <TouchableOpacity style={styles.buyButton} onPress={() => { }}>
+                <Image source={collectIcon} style={{width:12 ,height: 12}} />
+                <Text style={{
+                  color: globalStyle.colorPrimary
+                }}>关注</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
   _keyExtractor = (item, index) => item;
+
+  /**
+   * 下拉刷新
+   * @private
+   */
+  _onPullRelease(resolve) {
+    setTimeout(() => {
+      resolve();
+      this.moreTime = 0;
+    }, 3000);
+  }
+
+  /**
+  * 加载更多  数据加载
+  * @private
+  */
+  _loadMore() {
+  }
+
+  renderHotTabList() {
+    if (this.hotList == null) {
+      this.hotList = (
+        <FlatList
+          ref={(list) => this._listRef = list}
+          ListFooterComponent={this._footer}
+          ItemSeparatorComponent={this._separator}
+          data={this.state.movies}
+          renderItem={this._reanderItem}
+          keyExtractor={this._keyExtractor}
+          scrollEnabled={false} />
+      )
+    }
+    return this.hotList;
+  }
+
+  _commingSectionHeader({section}) {
+    return(
+      <View style={styles.commingSectionHeader}>
+        <Text style={{fontSize: 15, color: globalStyle.fontColorGray,}}>
+          {section.title}
+        </Text>
+      </View>
+    );
+  }
+
+  renderCommingTabList() {
+    if(this.commingList == null) {
+      this.commingList = (
+        <SectionList
+          ListFooterComponent={this._footer}
+          ItemSeparatorComponent={this._separator}
+          renderSectionHeader={this._commingSectionHeader}
+          sections={this.state.commingMovies}
+          renderItem={this._renderCommingMovieItem}
+          keyExtractor={(item, index)=>index}
+          scrollEnabled={false}
+        />
+      );
+    }
+    return this.commingList;
+  }
+
+  renderListWithTab() {
+    if (this.state.curTab == 0) return this.renderHotTabList();
+    else if(this.state.curTab == 1) return this.renderCommingTabList();
+  }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.homeContainer}>
         <Header tab={this.state.tab} changeSelect={(item) => this.changeSelect(item)} disableBack={true}></Header>
-        <View style={{
-          flex: 1
-        }}>
-          <FlatList ListHeaderComponent={this._header} ListFooterComponent={this._footer} ItemSeparatorComponent={this._separator} data={this.state.movies} renderItem={this._reanderItem} keyExtractor={this._keyExtractor} />
-        </View>
+        <RefreshScrollView
+          onPullRelease={(resolve) => this._onPullRelease(resolve)}
+        >
+          {this._header()}
+          {this.renderListWithTab()}
+        </RefreshScrollView>
       </View>
     )
   };
@@ -147,20 +324,24 @@ const styles = StyleSheet.create({
   },
   homeContainer: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: globalStyle.pageBackground
   },
   swiperContainer: {
     height: (0.5 * width) > 190 ? 190 : (0.5 * width),
-    overflow: 'hidden'
+    width: width,
   },
   slide: {
-    flex: 1,
+    /**
+     * 需要设置下height，不然下拉后图片变形
+     */
+    height: (0.5 * width) > 190 ? 190 : (0.5 * width),
+    width: width,
     justifyContent: 'center',
     alignItems: 'center',
   },
   adImage: {
     flex: 1,
-    width,
+    width: width,
   },
   pagination: {
     bottom: 8
@@ -172,7 +353,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingHorizontal: 15,
     paddingVertical: 10,
-    position: 'relative'
+    position: 'relative',
+    backgroundColor: globalStyle.backgroundColor
   },
   movieThumbContainer: {
     height: 80,
@@ -193,7 +375,7 @@ const styles = StyleSheet.create({
   },
   movieTitle: {
     fontSize: 16,
-    color: '#3f3f3f'
+    color: globalStyle.fontColorBlack
   },
   scoreNum: {
     fontSize: 14,
@@ -205,25 +387,35 @@ const styles = StyleSheet.create({
   },
   movieSlogan: {
     fontSize: 12,
-    color: '#3f3f3f'
+    color: globalStyle.fontColorBlack
   },
   movieActress: {
     fontSize: 12,
-    color: '#808080',
-    marginTop: 10
+    color: globalStyle.fontColorGray
   },
   buyButton: {
+    flexDirection: 'row',
     width: 46,
     height: 27,
     borderWidth: 1,
-    borderColor: '#dc3c38',
+    borderColor: globalStyle.colorPrimary,
     alignItems: 'center',
     justifyContent: 'center',
     borderRadius: 5,
     bottom: 0,
     position: 'absolute',
     right: 0
-  }
+  },
+  footer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 44
+  },
+  commingSectionHeader: {
+    paddingHorizontal: 15,
+    height: 30,
+    justifyContent: 'center'
+  },
 });
 
 module.exports = HomeScreen;
