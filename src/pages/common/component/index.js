6487c3deef636a10885f1ef33696824de036ec7c
linling <137391525@qq.com>
2017-10-09 15:46:24 +0800

    删除无用文件

--- src/pages/common/component/index.js
+++ src/pages/common/component/index.js
@@ -3,13 +3,11 @@ import ImageButton from './imagebutton';
 import Button from './button';
 import RatingView from './ratingView';
 import Tab from './tab';
-import ZoomImage from './zoomImage';
 import CFlatList from './cFlatList';
 module.exports = {
     ImageButton,
     Button,
     RatingView,
     Tab,
-    ZoomImage,
     CFlatList
 };
