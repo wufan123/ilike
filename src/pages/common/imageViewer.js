694abbf0dde2283630c8c31d2356b3c346d45176
Ashoka <savebtc11@gmail.com>
2017-10-09 10:42:07 +0800

    add back button to imager viewer

--- src/pages/common/imageViewer.js
+++ src/pages/common/imageViewer.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import {
     View,
-    Modal
+    Modal,
+    StyleSheet,
+    TouchableOpacity,
+    Image
 } from 'react-native';
 import PropTypes from 'prop-types';
 import ImageViewer from 'react-native-image-zoom-viewer';
 
 class ImageViewerScreen extends React.Component {
+
+    _renderBackButton() {
+        return (
+            <TouchableOpacity
+                style={styles.backButton}
+                onPress={() => this.props.navigation.goBack()}
+            >
+                <Image
+                    style={{ width: 18, height: 18 }}
+                    source={require('../../assets/common/back.png')}
+                />
+            </TouchableOpacity>
+        )
+    }
+
     render() {
         let images = this.props.images;
         try {
@@ -15,14 +33,33 @@ class ImageViewerScreen extends React.Component {
             
         }
         return (
-            <Modal visible={true} transparent={true}>
-                <ImageViewer
-                imageUrls={images.map((url)=>{return {url: url};})}
-                index={this.props.navigation.state.params.index}/>
-            </Modal>
+            <View style={{flex: 1}}>
+                <Modal visible={true} transparent={true}>
+                    {this._renderBackButton()}
+                    <ImageViewer
+                    imageUrls={images.map((url)=>{return {url: url};})}
+                    index={this.props.navigation.state.params.index}/>
+                </Modal>
+            </View>
         );
     }
 }
+
+styles = StyleSheet.create({
+    backButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        opacity: 0.5,
+        backgroundColor: '#000',
+        position: 'absolute',
+        top: 20,
+        left: 24,
+        zIndex: 20,
+    }
+});
 
 ImageViewerScreen.propTypes = {
     ...View.propTypes,
