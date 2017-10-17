7efb141f5e57e5b93aef8200180ee7f8b51911af
liuyueyuan <liuyueyuan@zrfilm.com>
2017-10-17 14:41:04 +0800

    退票成功

--- src/pages/me/ticketoperation/refundResult.js
+++ src/pages/me/ticketoperation/refundResult.js
@@ -15,18 +15,35 @@ class RefundRuleView extends Component {
   render() {
     return (
       <View >
-        <Header title={'退款成功'}  ></Header>
+        <Header title={'详情'}  ></Header>
+        <View style={[styles.view, {paddingBottom:30, marginBottom: 10,alignItems: 'center' }]}>
+        <Image source={require('../../../assets/me/success.png')} style={styles.imgSty} resizeMode='stretch'>
+        </Image>
+        <Text>订单退订成功！</Text>
+      </View>
         <View style={styles.view}>
-          <Text style={[theme.fontBlack,theme.mb10]}>退票规则说明</Text>
-          <Text>1.订单在影票开映前X小时可申请退票，超出时间范围不支持退票</Text>
-          <Text>2.订单在影票开映前X小时可申请退票，超出时间范围不支持退票</Text>
-          <Text>3.订单在影票开映前X小时可申请退票，超出时间范围不支持退票</Text>
-        </View>
-        <View style={styles.view}>
-          <Text style={[theme.fontBlack,theme.mb10]}>改签规则说明</Text>
-          <Text>1.订单在影票开映前X小时可申请退票，超出时间范围不支持退票</Text>
-          <Text>2.订单在影票开映前X小时可申请退票，超出时间范围不支持退票</Text>
-          <Text>3.订单在影票开映前X小时可申请退票，超出时间范围不支持退票</Text>
+          <View style={[styles.betweenView,theme.mb10,{ paddingVertical: theme.itemMargin, borderBottomWidth: 0.5, borderBottomColor: '#e8e8e8' }]}>
+          <Text style={[theme.fontBlack]}>退款明细</Text>
+          <Text style={[theme.fontBlack]}>订单总额 <Text style={theme.fontColorPrimary}>￥30</Text> 实付 <Text style={theme.fontColorPrimary}>￥15</Text> </Text>
+          </View>
+          <View style={styles.betweenView}>
+            <Text>影片名称</Text><Text>神奇女侠</Text> 
+          </View>
+          <View style={styles.betweenView}>
+            <Text>订单号</Text><Text>68598239856</Text> 
+          </View>
+          <View style={styles.betweenView}>
+            <Text>积分退回</Text><Text>300</Text> 
+          </View>
+          <View style={styles.betweenView}>
+            <Text>券退回</Text><Text>5元立减券1张</Text> 
+          </View>
+          <View style={styles.betweenView}>
+            <Text>积分退回</Text><Text>300</Text> 
+          </View>
+          <View style={styles.betweenView}>
+            <Text>券退回</Text><Text>5元立减券1张</Text> 
+          </View>
         </View>
         </View>
     )
@@ -34,7 +51,10 @@ class RefundRuleView extends Component {
 }
 
 const styles = StyleSheet.create({
-  view:{paddingHorizontal: theme.pagePadding,paddingVertical: theme.itemMargin,}
+  betweenView:{flexDirection: 'row',justifyContent: 'space-between'},
+  imgSty:{width:60,height:60,marginTop:30,marginBottom:10},
+  view:{paddingHorizontal: theme.pagePadding,justifyContent:'center',paddingVertical: theme.itemMargin,backgroundColor:'#ffffff',marginBottom:10},
+  center:{}
 })
 
 module.exports = RefundRuleView;
