66bbfb285473e7d947b072957393b01742f8eb9d
whr <357114638@qq.com>
2017-10-18 18:02:52 +0800

    扫码界面

--- src/pages/common/qrCodeScan.js
+++ src/pages/common/qrCodeScan.js
@@ -6,35 +6,36 @@ import React, {Component} from 'react';
 import {
     ToastAndroid,
     Text,
-    AppRegistry
+    AppRegistry, View, StatusBar
 } from 'react-native';
+import BasePage from "./basePage";
 export default class QrCodeScan extends Component {
     render() {
         return (
+            <View style={{flex:1}}>
+                <StatusBar
+                    hidden={true}
+                    barStyle={'light-content'}
+                />
             < QRScannerView
+                hintText={'将二维码放入条形框添加优惠券'}
+
                 onScanResultReceived={this.barcodeReceived.bind(this)}
 
                 renderTopBarView={() => this._renderTitleBar()}
 
                 renderBottomMenuView={() => this._renderMenu()}
             />
+            </View>
         )
     }
 
     _renderTitleBar(){
-        return(
-            <Text
-                style={{color:'white',textAlignVertical:'center', textAlign:'center',font:20,padding:12}}
-            >Here is title bar</Text>
-        );
+        return null;
     }
 
     _renderMenu() {
-        return (
-            <Text
-                style={{color:'white',textAlignVertical:'center', textAlign:'center',font:20,padding:12}}
-            >Here is bottom menu</Text>
-        )
+        return null
     }
 
     barcodeReceived(e) {
